2adaa1a14e626ff182e741047e7182b14dfc6faf
sutulo.vladlen <sutulo.vlad@gmail.com>
2022-11-27 19:21:17 +0300

    Example 2 in home work 2

--- Home_work/Home_work2/Program.cs
+++ Home_work/Home_work2/Program.cs
@@ -1,12 +1,28 @@
-﻿/*Программа узнает втрую цифру трехзначного число
+﻿//Программа узнает втрую цифру трехзначного число
 
-int namer = new Random().Next(100, 1000);
+/*int namer = new Random().Next(100, 1000);
 
 Console.WriteLine(namer);
 
 int a = (namer % 100) / 10;
 
 Console.WriteLine(a);
+*/
 
-
+// Нахождение третьей цифры любого числа
+/*int namber = int.Parse(Console.ReadLine());
+if (namber < 0)
+{
+    namber = namber * -1;
+}
+if (namber < 100)
+{
+    Console.WriteLine("Третьего числа нет");
+}
+else
+{
+    int therd_digit = namber % 1000 / 100;
+    Console.WriteLine(therd_digit);
+}
+*/
 

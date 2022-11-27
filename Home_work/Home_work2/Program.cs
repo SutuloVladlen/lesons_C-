f3f7b25791ab87c29ae91e0540ab6d72915e48d8
sutulo.vladlen <sutulo.vlad@gmail.com>
2022-11-27 18:30:32 +0300

    delit folder 2

--- Home_work/Home_work2/Program.cs
+++ Home_work/Home_work2/Program.cs
@@ -1,10 +1,12 @@
-﻿//Программа узнает втрую цифру трехзначного число
+﻿/*Программа узнает втрую цифру трехзначного число
 
-/*int namer = new Random().Next(100, 1000);
+int namer = new Random().Next(100, 1000);
 
 Console.WriteLine(namer);
 
 int a = (namer % 100) / 10;
 
-/*Console.WriteLine(a);
+Console.WriteLine(a);
+
+
 

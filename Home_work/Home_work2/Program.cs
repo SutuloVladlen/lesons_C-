5b87573e1077020e26eed9a79f864192d4a5237e
sutulo.vladlen <sutulo.vlad@gmail.com>
2022-11-27 17:36:01 +0300

    Example 1 in home work 2

---
+++ Home_work/Home_work2/Program.cs
@@ -0,0 +1,10 @@
+﻿//Программа узнает втрую цифру трехзначного число
+
+/*int namer = new Random().Next(100, 1000);
+
+Console.WriteLine(namer);
+
+int a = (namer % 100) / 10;
+
+/*Console.WriteLine(a);
+

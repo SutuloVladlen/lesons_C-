0b5d1b0a595146b874cc1df499d6e1bdd41e4550
sutulo.vladlen <sutulo.vlad@gmail.com>
2022-12-05 00:58:44 +0300

    hw4 exe3

---
+++ Home_work/Home_work4/exercise3/Program.cs
@@ -0,0 +1,27 @@
+﻿int nam = 8;
+int[] GetTebleOfCube(int nam)
+{
+    int[] arr = new int[nam];
+    for (int i = 0; i < nam; i++)
+    {
+        Random r = new Random();
+        int n = r.Next(0, 100);
+
+        arr[i] = n;
+    }
+    return arr;
+}
+
+int[] resalt = GetTebleOfCube(nam);
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < nam; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+
+}
+Console.Write("[");
+PrintArray(resalt);
+Console.Write("]");
+//немного не понял что конкретно нужно было сделать в задании, но надеюсь что сделал то.

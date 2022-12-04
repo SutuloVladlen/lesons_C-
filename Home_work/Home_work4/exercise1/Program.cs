d656413456bf60cee7d1e342ca7b844b745cc4b5
sutulo.vladlen <sutulo.vlad@gmail.com>
2022-12-04 19:41:46 +0300

    hw4 exe1

---
+++ Home_work/Home_work4/exercise1/Program.cs
@@ -0,0 +1,31 @@
+﻿
+Console.WriteLine("Введите число которое возводим в степень");
+
+bool namberA = int.TryParse(Console.ReadLine(), out int a);
+
+Console.WriteLine("Введите степень в которую возводим");
+
+bool namberB = int.TryParse(Console.ReadLine(), out int b);
+
+if (!namberA && !namberB)
+{
+    Console.WriteLine("Ввели не корректно данные");
+    return;
+}
+
+
+double exponentiation = Exponentiation(a, b);
+
+Console.WriteLine(exponentiation);
+
+double Exponentiation(int a, int b)
+{
+    double expon = 1;
+    for (int i = 1; i <= b; i++)
+
+    {
+        expon = expon * a;
+    }
+    return expon;
+
+}

0b62227589796222c577bf9e310d42796aa3e6ea
sutulo.vladlen <sutulo.vlad@gmail.com>
2022-12-07 18:00:11 +0300

    hw 5 exe1 and exe2

---
+++ Home_work/Home_work5/exercise1/Program.cs
@@ -0,0 +1,48 @@
+﻿int[] array = GenerateArray(100, 999, 20);
+PrintArray(array);
+
+int positiv_nambers = QuantityPositivNanbers(array);
+Console.WriteLine(positiv_nambers);
+
+int QuantityPositivNanbers(int[] array)
+{
+    int caunt = 0;
+    for (var i = 0; i < array.Length; i++)
+    {
+
+        int n = array[i];
+        if (n % 2 == 0)
+            caunt++;
+
+    }
+    return caunt;
+}
+
+int[] GenerateArray(int min, int max, int length)
+{
+    Random random = new Random();
+    int[] array = new int[length];
+
+    for (var i = 0; i < array.Length; i++)
+    {
+        array[i] = random.Next(min, max + 1);
+    }
+
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    // Console.Write("[");
+    // for (var i = 0; i < array.Length; i++)
+    // {
+
+    //     Console.Write($"{array[i]},");
+
+    // }
+    // Console.Write("]");
+    string message = string.Join(" ,", array);
+    Console.WriteLine($"[{string.Join(" ,", array)}]");
+
+    //Console.WriteLine("[{0}]", string.Join(", ", array));
+}
---
+++ Home_work/Home_work5/exercise2/Program.cs
@@ -0,0 +1,46 @@
+﻿int[] array = GenerateArray(-10, 10, 10);
+PrintArray(array);
+
+int sum = SumElementsArrayWethOddIndex(array);
+Console.WriteLine(sum);
+
+int SumElementsArrayWethOddIndex(int[] array)
+{
+    int summ = 0;
+    for (var i = 0; i < array.Length; i++)
+    {
+        if (i % 2 == 1)
+            summ = summ + array[i];
+
+    }
+    return summ;
+}
+
+int[] GenerateArray(int min, int max, int length)
+{
+    Random random = new Random();
+    int[] array = new int[length];
+
+    for (var i = 0; i < array.Length; i++)
+    {
+        array[i] = random.Next(min, max + 1);
+    }
+
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    // Console.Write("[");
+    // for (var i = 0; i < array.Length; i++)
+    // {
+
+    //     Console.Write($"{array[i]},");
+
+    // }
+    // Console.Write("]");
+    string message = string.Join(" ,", array);
+    Console.WriteLine($"[{string.Join(" ,", array)}]");
+
+    //Console.WriteLine("[{0}]", string.Join(", ", array));
+}

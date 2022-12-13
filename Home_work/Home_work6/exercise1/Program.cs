1ad5a783130d51bc0bc6abb78e98a34b1cbbd682
sutulo.vladlen <sutulo.vlad@gmail.com>
2022-12-13 16:34:41 +0300

    hw6 exe1

---
+++ Home_work/Home_work6/exercise1/Program.cs
@@ -0,0 +1,38 @@
+﻿Console.WriteLine("Введите количество чисел которое будем проверять");
+int m = int.Parse(Console.ReadLine());
+
+int[] array = CreationArray(m);
+
+// PrintArray(array);
+
+int positive_nam = AmountPositiveNambers(array);
+Console.Write($"Количество положительных чисел равно {positive_nam}");
+
+int[] CreationArray(int m)
+{
+    int[] array = new int[m];
+    for (int i = 0; i < m; i++)
+    {
+        array[i] = int.Parse(Console.ReadLine());
+    }
+    return array;
+}
+
+int AmountPositiveNambers(int[] array)
+{
+    int positive_nam = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            positive_nam++;
+        }
+    }
+    return positive_nam;
+}
+
+// void PrintArray(int[] array);
+// {
+//     Console.WriteLine("[{0}]", string.Join(", ", array));
+// }
+// Немогу понять почему он не хочет вывести массив, если подскажите будет великолепно.
